da804d9191ace7fcf30d016c72478d91ce455874
riain <ryanftoal@gmail.com>
2025-03-01 18:20:44 +0000

    Added CSV documentation

--- csv.h
+++ csv.h
@@ -1,3 +1,17 @@
+/**
+ * CSV Parser v1.0
+ *
+ * Provides utilities for reading, editing and writing CSV.
+ *
+ *
+ * This parser cannot extend or retract rows or columns yet.
+ * Values can only be modified in place.
+ *
+ * Riain Ó Tuathail
+ * 01-03-2025
+ *
+ */
+
 #pragma once
 
 #include <stdbool.h>
@@ -35,19 +49,24 @@ typedef struct ValueCSV
 } ValueCSV;
 
 
+// Allocate a CSV parser that must be freed with destroy_csv()
 CSV *create_csv(void);
 
+// Destroy an allocated CSV parser, gracefully handles NULL CSV pointers
 void destroy_csv(CSV **csv);
 
 
+// Parse a CSV file
 void parse_csv(CSV *csv,
                const char *filepath);
 
-
+// Output contents of CSV parser to file
 void write_csv(const CSV *csv,
                const char *filepath);
 
 
+// Retrieve an editable value from the opaque CSV parser, returns NULL on failure
+// NOTE: modification to this value struct will be outputted to file on write_csv() call
 ValueCSV *get_csv(const CSV *csv,
                   size_t column,
                   size_t row);
