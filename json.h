665797138301181f474da049d21c5402488e9191
riain <ryanftoal@gmail.com>
2025-05-24 15:02:45 +0100

    Updated documentation for doxygen

--- json.h
+++ json.h
@@ -1,11 +1,12 @@
 /**
- * JSON Parser v1.0
+ * @file json.h
+ * @brief JSON Parser
  *
  * Provides utilities for reading, editing and writing JSON.
  *
- * Riain Ó Tuathail
- * 01-03-2025
- *
+ * @author Riain Ó Tuathail
+ * @date 2025-05-24
+ * @version v1.1
  */
 
 #pragma once
@@ -33,13 +34,13 @@ typedef enum HintJSON
 
 typedef union TypeJSON
 {
-    void    *n;
-    char    *s;
-    long     i;
-    double   f;
-    bool     b;
-    Object  *o;
-    Array   *a;
+    void   *n;
+    char   *s;
+    long    i;
+    double  f;
+    bool    b;
+    Object *o;
+    Array  *a;
 } TypeJSON;
 
 typedef struct Value
@@ -49,57 +50,58 @@ typedef struct Value
 } ValueJSON;
 
 
-// Allocate a JSON parser that must be freed with destroy_json()
+/// Allocate a JSON parser that must be freed with destroy_json()
+__attribute__((warn_unused_result))
 JSON *create_json(void);
 
-// Destroy an allocated JSON parser, gracefully handles NULL JSON pointers
+/// Destroy an allocated JSON parser, gracefully handles NULL JSON pointers
 void destroy_json(JSON **json);
 
 
-// Parse a JSON file
+/// Parse a JSON file
 void parse_json(JSON *json,
                 const char *path);
 
-// Output contents of JSON parser to file
+/// Output contents of JSON parser to file
 void write_json(const JSON *value,
                 const char *path);
 
 
-// Retrieve an editable value from the opaque JSON parser, returns NULL on failure
+/// Retrieve an editable value from the opaque JSON parser, returns NULL on failure
 ValueJSON *get_json(JSON *json);
 
-// Retrieve a value from a JSON Object, returns NULL on failure
+/// Retrieve a value from a JSON Object, returns NULL on failure
 ValueJSON *lookup_json(ValueJSON *object,
                        const char *key);
 
-// Retrieve a value from a JSON Array, returns NULL on failure
+/// Retrieve a value from a JSON Array, returns NULL on failure
 ValueJSON *scan_json(ValueJSON *array,
                      size_t index);
 
 
-// Allocate a value with the JSON parser's allocator, returns default value
+/// Allocate a value with the JSON parser's allocator, returns default value
 ValueJSON *make_json(JSON* json,
                      HintJSON hint);
 
 
-// Insert a value into a JSON Object
+/// Insert a value into a JSON Object
 void insert_json(ValueJSON *object,
                  const char *key,
                  ValueJSON *value);
 
-// Erase a value from a JSON object
+/// Erase a value from a JSON object
 void erase_json(ValueJSON *object,
                 const char *key);
 
 
-// Push back a value into a JSON Array
+/// Push back a value into a JSON Array
 void push_json(ValueJSON *array,
                ValueJSON *value);
 
-// Pop a value at the index of a JSON Array
+/// Pop a value at the index of a JSON Array
 void pop_json(ValueJSON *array,
               size_t index);
 
 
-// Get the number of values in a JSON Object or Array, returns SIZE_MAX on failure
+/// Get the number of values in a JSON Object or Array, returns SIZE_MAX on failure
 size_t count_json(ValueJSON* container);
